4412a0d61c1d41e9ee3623171aab96338eeb3b12
JR Cologne <kontakt@jr-cologne.de>
2019-08-23 13:39:29 +0200

    Bump versions

--- app/classes/Exceptions/CryptoClientException.php
+++ app/classes/Exceptions/CryptoClientException.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * @author JR Cologne <kontakt@jr-cologne.de>
  * @copyright 2019 JR Cologne
  * @license https://github.com/jr-cologne/CryptoStatus/blob/master/LICENSE MIT
- * @version v0.8.2-beta
+ * @version v0.8.3-beta
  * @link https://github.com/jr-cologne/CryptoStatus GitHub Repository
  *
  * ________________________________________________________________________________
