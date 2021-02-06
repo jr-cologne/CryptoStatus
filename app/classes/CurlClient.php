2bfd4cbc5b874dba34484f6d87aeeb04adb0ae4e
JR Cologne <kontakt@jr-cologne.de>
2021-02-06 20:31:58 +0100

    bump versions

--- app/classes/CurlClient.php
+++ app/classes/CurlClient.php
@@ -3,14 +3,14 @@
 /**
  * A simple Twitter bot application which posts hourly status updates for the top 10 cryptocurrencies.
  *
- * PHP version >= 7.1
+ * PHP version >= 7.4
  *
  * LICENSE: MIT, see LICENSE file for more information
  *
  * @author JR Cologne <kontakt@jr-cologne.de>
- * @copyright 2019 JR Cologne
+ * @copyright 2021 JR Cologne
  * @license https://github.com/jr-cologne/CryptoStatus/blob/master/LICENSE MIT
- * @version v0.8.3-beta
+ * @version v0.9.0-beta
  * @link https://github.com/jr-cologne/CryptoStatus GitHub Repository
  *
  * ________________________________________________________________________________
